6d17d652c354187a21657f15074ff400e8a2d515
Rohit sahu <quantumsolitaryseeker@gmail.com>
2025-12-12 19:00:30 +0530

    Update add two number.py

--- basic math/add two number.py
+++ basic math/add two number.py
@@ -1,2 +1,15 @@
-#  Program to Add Two Number 
+#  Write a Python program that takes two numbers as input from the user and prints their sum.
 
+a = float(input("Enter the first number: "))
+b = float(input("Enter the second number: "))
+
+result = a + b
+
+print("Sum of two numbers is:", result)
+
+"""
+# Output 
+Enter the first number: 5
+Enter the second number: 7
+Sum of two numbers is: 12.0
+"""
