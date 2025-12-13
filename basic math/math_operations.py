92f489f270d3d93c9e79f4f70f4650aad5fbfa1c
Rohit sahu <quantumsolitaryseeker@gmail.com>
2025-12-13 20:23:24 +0530

    Implement basic math operations in Python
    Added a Python script for basic math operations including addition, subtraction, multiplication, division, floor division, modulus, and power.

--- basic math/math_operations.py
+++ basic math/math_operations.py
@@ -1,1 +1,42 @@
 
+# SIMPLE MATH OPERATIONS IN PYTHON 
+
+"""  
+PERFORM Addition , Subtraction , Multiplication , Division , Floor Division , Modulus , Power ,
+
+"""
+
+a = 23
+b = 7 
+# Addition 
+print("Addition:", a + b)
+
+# Subtraction
+print("Subtraction:", a - b)
+
+# Multiplication
+print("Multiplication:", a * b)
+
+# Division (float result)
+print("Division:", a / b)
+
+# Floor Division (integer result)
+print("Floor Division:", a // b)
+
+# Modulus (remainder)
+print("Modulus:", a % b)
+
+# Power / Exponent
+print("Power:", a ** b)
+
+
+"""
+# OUTPUT 
+Addition: 30
+Subtraction: 16
+Multiplication: 161
+Division: 3.2857142857142856
+Floor Division: 3
+Modulus: 2
+Power: 3404825447
+"""
