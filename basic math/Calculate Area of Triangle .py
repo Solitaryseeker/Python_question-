0ed2f4510b32f3cdc9ca4aceacff3422e66dcece
Rohit sahu <quantumsolitaryseeker@gmail.com>
2025-12-14 23:13:19 +0530

    Update program to calculate area of triangle

---
+++ basic math/Calculate Area of Triangle .py
@@ -0,0 +1,8 @@
+#  Calculate Area of Triangle in Python  
+
+length = float(input("Enter the length of the triangle: "))
+breadth = float(input("Enter the breadth of the triangle: "))
+
+area = 0.5 * length * breadth
+
+print("Area of the triangle:", area) 
